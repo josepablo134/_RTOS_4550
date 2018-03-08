7e4c79828bbbb08664f60973e8de18eadafeca76
josepablo134 <josepablo134@gmail.com>
2018-03-07 21:50:46 -0600

    Scheduler Version 1

--- RTOS.c
+++ RTOS.c
@@ -1,152 +1,172 @@
 #include "RTOS.h"
 
-volatile unsigned char      Scheduler_flag=0;
-volatile unsigned char      TaskCounter=0;
-volatile unsigned char       SchBSR=0;
-volatile unsigned char       SchWREG=0;
-volatile unsigned char       SchSTATUS=0;
-volatile unsigned char long SchPC=0;
-volatile unsigned char      SchPCL=0;
-volatile unsigned char      SchPCH=0;
-volatile unsigned char      SchPCU=0;
-
-//StackStruct RTOS_STACK[31];
-thread*  Scheduler_Tasks[MAX_THREAD];//Arreglo de apuntadores a thread
-
-void setup_rtos() {
-    INTCONbits.GIE = INTCONbits.PEIE = 1;
-    INTCONbits.TMR0IE = INTCON2bits.T0IP=1;
-    T0CON=0b00000000;TMR0 = DefaultTimerData;
-//    for(unsigned char i=0;i<31;i++){
-//        RTOS_STACK[i] = 0x00;//Limpiar pila de sistema
-//    }
-}
-
-void Task_execute(thread* Task){
-    asm("POP");//Ignorar contexto anterior!
-    if(Task->state & READY){
-        Task->function();
+void init_tasks(){
+    //  Guardar el contexto del main u origen de ejecución
+    rtos.main_task.wreg     = WREG;
+    rtos.main_task.status   = STATUS;
+    rtos.main_task.bsr      = BSR;
+    rtos.main_task.pcu      = TOSU;
+    rtos.main_task.pch      = TOSH;
+    rtos.main_task.pcl      = TOSL;
+    //Guardar el contexto del administrador de tareas
+    asm("PUSH");
+    if(rtos.first_exec){
+        rtos.manager_PC_U = TOSU;
+        rtos.manager_PC_H = TOSH;
+        rtos.manager_PC_L = TOSL-2;
+        //
+        //  Con esto retornaremos hasta el push
+        //  y evitaremos que pop underborde la pila
+        //
+        TMR0=0;
+        T0CONbits.TMR0ON = 1;//Activamos el timer
+        rtos.first_exec = false;
     }
-    Task->state = DONE;//Tarea finalizada!
-    SoftwareInterrupt();//Cambiar de tarea!
-    while(1);
-}
-
-void Scheduler(){
-    asm("PUSH");//Aqui comienza el llamado al Scheduler desde la interrupcion
-    if(!Scheduler_flag){
-        //Guardar el estado de esta función
-        SchPC        = TOS;
-        
-        TaskCounter     = 0;
-        Scheduler_flag  = 1;//Evitar repetir inicializacion
-    }
-    asm("POP");//Ignorar Contexto anterior!
-SCHEDULER_LOOP:
-//El sistema debe estar listo para conmutar entre funciones
-//La pila debe estar completamente vacia
-
-    //Preparar siguiente ejecucion
-    T0CONbits.TMR0ON = 1;//Comenzar tiempo de tarea!
-    if(Scheduler_Tasks[TaskCounter]->state & READY){
-        //No ha sido ejecutada
-        Task_execute(Scheduler_Tasks[TaskCounter]);
-    }else if(Scheduler_Tasks[TaskCounter]->state & BLOCKED){
-        //Continuar ejecucion
-            //Cargar pila de la tarea!
-            for(unsigned i=0;i<Scheduler_Tasks[TaskCounter]->StackPtr;i++){
-                TOS     = Scheduler_Tasks[TaskCounter]->Stack[i];
-                asm("PUSH");
-            }
-            TOS = Scheduler_Tasks[TaskCounter]->pc;//Cargar retorno!
-            
-            //Cargar el contexto de la tarea
-            STATUS  = Scheduler_Tasks[TaskCounter]->status;
-            BSR     = Scheduler_Tasks[TaskCounter]->bsr;
-            WREG    = Scheduler_Tasks[TaskCounter]->wreg;
-            //Regresar a la ejecucion
-            return;
-    }//Cualquier opcion omite la tarea
-    TaskCounter++;
-    TaskCounter%=MAX_THREAD;
-    goto SCHEDULER_LOOP;
-}
-
-void Task_init(thread* Task, void (*thread_function)(void) , StackStruct* SStruct, StackSize SSize){
-    Task->bsr       = 0x00;
-    Task->pc        = 0x00;
-    Task->state     = READY;//Tarea lista para ser ejecutada
-    Task->status    = 0x00;
-    Task->wreg      = 0x00;
-    Task->function  = thread_function;
-    Task->Stack     = SStruct;
-    Task->StackSize = SSize;
-    Task->StackPtr  = 0;//StackPtr
-}
-
-void Task_register(thread* Task){
-    if(TaskCounter<MAX_THREAD){
-        Scheduler_Tasks[TaskCounter] = Task;
-        TaskCounter++;
-    }
-}
-
-void interrupt HISR(){
-//La interrupcion de alta prioridad se ejecuta cuando haya
-//habido una interrupcion y se esten ejecutando tareas
-//Por lo tanto, al llegar aqui se llega desde el contexto
-//de una tarea
-    SchWREG     = WREG;//Capturar WREG y registros de contexto
-    SchBSR      = BSR;
-    SchSTATUS   = STATUS;
-//Apagar el timer
-    T0CONbits.TMR0ON = 0;
-    // Limpiar el wdt
+    asm("POP");
     asm("CLRWDT");
-//Recuperar contexto en la tarea actual
-    unsigned char   STKPTR_buffer=0;
-    STKPTR_buffer = STKPTR;
-    Scheduler_Tasks[TaskCounter]->StackPtr = STKPTR_buffer;
-    while(STKPTR_buffer>1){//Mientras hayan elementos en la pila
-        Scheduler_Tasks[TaskCounter]->Stack[STKPTR_buffer] = TOS;//POP desde pila!
-        asm("POP");
-        STKPTR_buffer = STKPTR;
+    //
+    //Primer lanzamiento de tareas
+    //
+THREAD_TEST:
+    if(launcher_mode){
+        rtos.task_counter++;
+        if(rtos.task_counter <= MAX_THREADS){
+                actual_thread = &rtos.task[ rtos.task_counter-1 ];
+                task_father(&rtos.task[ rtos.task_counter -1 ]);
+                asm("GOTO $");
+        }else{
+            launcher_mode = false;
+            rtos.task_counter = 0;
+            actual_thread = &rtos.main_task;
+            task_recovery(&rtos.main_task);
+            asm("GOTO $");
+        }
+    }else{
+        rtos.task_counter++;
+        if(rtos.task_counter <= MAX_THREADS){
+            actual_thread = &rtos.task[ rtos.task_counter-1 ];
+            if(rtos.task[ rtos.task_counter-1 ].life){
+                //Si la tarea tiene vida se ejecuta
+                task_recovery(&rtos.task[ rtos.task_counter-1 ]);
+                asm("GOTO $");
+            }else{
+                //Sino se prueba con la siguiente
+                TMR0=0;
+                goto THREAD_TEST;
+            }
+        }else{
+            rtos.task_counter=0;
+            if(!rtos.task[0].life &&\
+                        !rtos.task[1].life &&\
+                        !rtos.task[2].life &&\
+                        !rtos.task[3].life){
+                    goto ALL_DEATH;
+            }else{
+                    actual_thread = &rtos.main_task;
+                    task_recovery(&rtos.main_task);
+                    asm("GOTO $");
+            }
+        }
     }
-    //Copiar registros de contexto!
-    Scheduler_Tasks[TaskCounter]->bsr   = SchBSR;
-    Scheduler_Tasks[TaskCounter]->wreg  = SchWREG;
-    Scheduler_Tasks[TaskCounter]->status= SchSTATUS;
-    
-    //Ejecutar Interrupt Service Runtime
-    if(hisr){
+ALL_DEATH:
+    T0CONbits.TMR0ON = 0;
+    //
+    //  Recuperar contexto del main
+    //
+    task_recovery(&rtos.main_task);
+}
+void interrupt HISR(){
+    if(*hisr){
         hisr();
     }
-//De tratarse de una interrupcion de OS
     if(TMR0IF){
-        // Limpiar la interrupción
+        //
+        //Vaciar pila excepto los primeros 2 elementos
+        //
+            actual_thread->wreg     = WREG;
+            actual_thread->bsr      = BSR;
+            actual_thread->status   = STATUS;
+            actual_thread->pcl      = TOSL;
+            actual_thread->pch      = TOSH;
+            actual_thread->pcu      = TOSU;
+            //
+            //  AGREGAR A LA PILA
+            //
+            
+            //
+            //  
+            //
+        global_pcu    = rtos.manager_PC_U;
+        global_pch    = rtos.manager_PC_H;
+        global_pcl    = rtos.manager_PC_L;
+        
         TMR0IF = 0;
-        // Regresar al controlador de tareas
-            //Omitimos el valor cargado en la pila
-        #asm
-            MOVFF ,TOSL
-            MOVFF ,TOSH
-            MOVFF ,TOSU
-        #endasm
-        return;
-    }else{
-//De haber sido la ejecucion de una interrupcion
-//durante una tarea restaurar contexto y regresar a la tarea
-        STKPTR = Scheduler_Tasks[TaskCounter]->StackPtr;
-        // Limpiar el wdt
-        asm("CLRWDT");
-        T0CONbits.TMR0ON = 1;//Continuar tiempo de ejecucion
-        return;
+        
+        STATUS = global_status;
+        BSR = global_bsr;
+        TOSU = global_pcu;
+        asm("MOVF _global_pcu,W");
+        asm("MOVWF TOSU");
+        asm("MOVF _global_pch,W");
+        asm("MOVWF TOSH");
+        asm("MOVF  _global_pcl,W");
+        asm("MOVWF TOSL");
+//        asm("RETFIE");
     }
 }
-
 void interrupt low_priority LISR(){
-    T0CONbits.TMR0ON = 0;//Apagar el timer
     if(lisr){
         lisr();
+    }
+}
+void task_father(thread* task){
+    if(task->function){
+        task->life = true;
+        task->function();
+        task->life = false;
+    }
+}
+void task_recovery(thread* task){
+        //
+        //Recuperar pila
+        //
+        global_status = task->status;
+        global_bsr    = task->bsr;
+        global_wreg   = task->wreg;
+        global_pcl    = task->pcl;
+        global_pch    = task->pch;
+        global_pcu    = task->pcu;
+//        STATUS = global_status;
+        asm("MOVF _global_status,W");
+        asm("MOVWF  STATUS");
+//        BSR = global_bsr;
+        asm("MOVF _global_bsr,W");
+        asm("MOVWF  BSR");
+//        TOSU = global_pcu;
+        asm("MOVF _global_pcu,W");
+        asm("MOVWF TOSU");
+        asm("MOVF _global_pch,W");
+        asm("MOVWF TOSH");
+        asm("MOVF  _global_pcl,W");
+        asm("MOVWF TOSL");
+        asm("MOVF  _global_wreg,W");
+        asm("RETURN");
+}
+void set_task(void (*function)(),unsigned char task){
+    switch(task){
+        case 0:
+            rtos.task[0].function = function;
+            break;
+        case 1:
+            rtos.task[1].function = function;
+            break;
+        case 2:
+            rtos.task[2].function = function;
+            break;
+        case 3:
+            rtos.task[3].function = function;
+            break;
+        default:
+            return;
     }
 }
--- main.c
+++ main.c
@@ -1,37 +1,41 @@
+#include "pic_configuration.h"
 #include "RTOS.h"
-#define     MAXSTACKSIZE    31
-#define     EXEC_DONE    {while(1){}}
+#define     DONE    {while(1){}}
+unsigned char __delay_counter1_;
+unsigned char __delay_counter2_;
+#define  simple_delay(){for(__delay_counter1_=0;\
+                            __delay_counter1_<255;\
+                            __delay_counter1_++){\
+                                for(__delay_counter2_=0;\
+                                __delay_counter2_<255;\
+                                __delay_counter2_++){}\
+                            }}
 
-#define  simple_delay(){for(unsigned char i=0;i<255;i++){}}
-
-
-StackStruct    Task0Stack[MAXSTACKSIZE];
-StackStruct    Task1Stack[MAXSTACKSIZE];
-thread task0;
-thread task1;
-
+void interrupt_hp();
+void toggleA0();
+void toggleA4();
 void task_0();
 void task_1();
 
 void main(void) {
     setup_rtos();
-    
-    Task_init(&task0,task_0,Task0Stack,MAXSTACKSIZE);
-    Task_init(&task1,task_1,Task1Stack,MAXSTACKSIZE);
-    
-    Task_register(&task0);
-    Task_register(&task1);
-
-    Scheduler();
-    EXEC_DONE;
+    //set_high_interrupt_vector(interrupt_hp);
+    set_task(task_0,0);
+    set_task(task_1,1);
+    init_tasks();
+    DONE;
 }
 
+void interrupt_hp(){
+    return;
+}
 void task_0(){
     PORTA = 0;
     LATA = 0;
     TRISA = 0;
     ADCON1 = 0x0F;
     CMCON = 0x07;
+    volatile unsigned char a=0x20; 
     while(1){
         LATAbits.LA4 = 1;
         simple_delay();
@@ -39,7 +43,6 @@ void task_0(){
         simple_delay();
     }
 }
-
 void task_1(){
     PORTA = 0;
     LATA = 0;
@@ -47,9 +50,9 @@ void task_1(){
     ADCON1 = 0x0F;
     CMCON = 0x07;
     while(1){
+        simple_delay();
         LATAbits.LA0 = 1;
         simple_delay();
         LATAbits.LA0 = 0;
-        simple_delay();
     }
 }
